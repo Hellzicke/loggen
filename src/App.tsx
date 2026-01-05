c16c86ec01350f536a6ee51e37ad6787b370714d
CodeRed <realhellzicke@gmail.com>
2026-01-05 09:35:33 +0100

    Remove useMemo import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useMemo } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import Header from './components/Header'
 import LogForm from './components/LogForm'
 import LogList from './components/LogList'
